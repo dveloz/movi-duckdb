5ca8743d6ad267272f348d2fa924713fd4103604
dveloz <dvdveloz12@gmail.com>
2023-05-02 22:16:14 -0600

    redesigned to work with s3

--- duck_setup.py
+++ duck_setup.py
@@ -1,52 +1,86 @@
 import duckdb
 from pathlib import Path
 import os
-# ToDo
-# setup for s3 (modules and shit)
-# View generator
+from typing import List
+from s3path import S3Path
+import logging
+
 
 class DuckDBBuilder:
     """Builds a DuckDB instance that uses data allocated in a S3 instance.
     Make sure to close your Database viewer.
     """
     def __init__(self) -> None:
-        self.facts_path = self.get_facts_path()
+        self.access_key = input("Escribe tu AWS_ACCESS_KEY_ID: ")
+        self.secret_key = input("Escribe tu AWS_SECRET_ACCESS_KEY: ")
+        self.facts_path = self._get_facts_path()
+        self.duck_db_path = Path().home().joinpath('movicar-duckdb')
+        self.logger = logging.getLogger("MoviDuckDBInstaller")
 
 
-    def get_facts_tables(self):
-        return os.listdir(self.facts_path)
-
-    
-    def get_facts_path(self) -> Path:
-        """Returns the path where the data is located
+    def _crawl_tables_paths(self) -> List[str]:
+        """Returns a list of directories containing tables.
 
         Returns:
-            Path: _description_
+            List[str]: List of table names.
         """
-        if os.getenv("MODE", "dev") == "prod":
-            # ToDo
-            return None
-        else:
-            return Path(__file__).parent.joinpath('tmp', 'analytics-dev', 
-                                                  'facts')
-        
-    
-    def generate_views(self, conn:duckdb.DuckDBPyConnection) -> None:
-        sql_template = "CREATE OR REPLACE VIEW {table} \
-                        AS SELECT * FROM '{path}/*.parquet';"
-        for table in self.get_facts_tables():
-            table_path = self.facts_path.joinpath(table)
-            conn.execute(sql_template.format(table=table, path=table_path))
+        return list(self.facts_path.iterdir())
 
+    
+    def _get_facts_path(self) -> Path:
+        """Returns the path where the 'facts' data is located
+
+        Returns:
+            Path: Path with of the 'facts'.
+        """
+        return S3Path(f'/movi-data-lake').joinpath("analytics-prod", "facts")
+    
+    
+    def _setup_views(self, conn:duckdb.DuckDBPyConnection) -> None:
+        """Creates a set of Views in a given DuckDB instance.
+
+        Args:
+            conn (duckdb.DuckDBPyConnection): A connection to the DuckDB 
+            instance where the views will be created.
+        """
+        self.logger.info("Exploring Movicar data...")
+        sql_template = """CREATE OR REPLACE VIEW {table}
+                          AS SELECT * FROM '{path}/*.parquet';"""
+        for table_path in self._crawl_tables_paths():
+            conn.execute(sql_template.format(table=table_path.name, 
+                                             path=table_path.as_uri()))
+
+
+    def _load_s3_deps(self, conn:duckdb.DuckDBPyConnection) -> None:
+        """ Executes the necessary commands to create a connection to S3.
+        """
+        self.logger.info("Installing DuckDB dependencies...")
+        conn.execute("INSTALL httpfs;")
+        conn.execute("LOAD httpfs;")
+        conn.execute("SET s3_region='us-west-1';")
+    
+        conn.execute(f"SET s3_access_key_id='{self.access_key}';")
+        conn.execute(f"SET s3_secret_access_key='{self.secret_key}';")
+        
 
     def build_duck_db(self):
-        conn = duckdb.connect(database=str(Path(__file__).parent.
-                            joinpath('movi-analytics.duckdb')), read_only=False)
-        self.generate_views(conn)
+        """Runner that setups a Movicar DuckDB instance.
+        """
+        self.logger.info("Creating DuckDB instance...")
+        os.makedirs(self.duck_db_path, exist_ok=True)
+        conn = duckdb.connect(
+                 database=str(self.duck_db_path.joinpath('analytics-prod.duckdb')),
+                 read_only=False
+                )
+        self._load_s3_deps(conn)
+        self._setup_views(conn)
+        self.logger.info("DuckDB setup correctly")
 
 
 if __name__ == '__main__':
     """Executes the builder
     """
+    logging.basicConfig(level=logging.INFO)
+
     DuckDBBuilder().build_duck_db()
 
