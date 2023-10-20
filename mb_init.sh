f70048b954f7e33f14cb8fd65756db7ed05d8a59
dveloz2 <david.veloz@klar.mx>
2023-10-20 12:14:10 -0600

    pushed new image

--- mb_init.sh
+++ mb_init.sh
@@ -15,12 +15,3 @@ echo "DuckDB builder execution complete."
 
 echo "Starting Metabase..."
 
-java -jar /home/metabase.jar >> logs.txt 2>&1
-return_code=$?
-
-if [ $return_code -eq 0 ]; then
-    echo "Metabase executed successfully."
-else
-    echo "Metabaseapplication encountered an error with return code $return_code."
-fi
-
